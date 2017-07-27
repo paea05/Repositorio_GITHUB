ee85547c42ad0f6e2e19a26d1fa9d1b90da7e99f
ite <ite@172.81.2.132>
2017-07-27 13:53:48 -0500

    Primer Commit

---
+++ ham-msd/src/main/java/com/msd/ham/dao/dtosCuantEcoDAO.java
@@ -0,0 +1,8 @@
+package com.msd.ham.dao;
+
+import com.msd.ham.models.dtosCuantEco;
+
+public interface dtosCuantEcoDAO {
+	public String consGram(String nG);
+	public int insertDtCuant(dtosCuantEco dCu);
+}
